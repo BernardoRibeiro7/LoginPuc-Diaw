a54a14601f77caaca43bac56592336b7c990ce7c
Bernardo Ribeiro <ribeiromachadobernardo@gmail.com>
2026-09-21 11:52:57 -0300

    chore: cria estrutura inicial do projeto Spring Boot

---
+++ src/test/java/br/com/puc/loginpuc/LoginpucApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.puc.loginpuc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginpucApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
